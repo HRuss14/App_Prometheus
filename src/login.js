379c721e93fbae96e5ada4dbaaa04982ee97e111
juan2409 <gamingjdc1@gmail.com>
2022-05-18 07:52:57 -0500

    Update 1

--- src/login.js
+++ src/login.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import Helmet from "react-helmet";
 import logoGrande from './Logo_Grande.svg'
 import Boton from "./elementos/Boton";
 import './App.css';
 
 const InicioSesion = () =>{
+
+const [mail, changeMail] = useState("User");
+const [password, changePassword] = useState("User");
     return(
     <>
         <Helmet>
@@ -13,10 +16,10 @@ const InicioSesion = () =>{
         <img class="logo_grande" src={logoGrande} style={{marginTop:'90px'}}/>
         <form style={{ position:'absolute', marginTop:'520px', alignItems:'center'}} >
             <input class="inputs"style={{display:'flex',alignItems:'flex-start', padding:'8px 12px', width:'471px',height:'40px', background:'#ffffff'}} 
-            type="email" placeholder="Correo"></input>
+            type="email" onChange={e => changeMail(e.target.value)} placeholder="Correo"></input>
             <br></br>
             <input class="inputs" style={{ marginTop:'40px',display:'flex',alignItems:'flex-start', padding:'8px 12px', width:'471px',height:'40px', background:'#ffffff'}}
-            type="password" placeholder="Contraseña"></input>
+            type="password" onChange={e => changePassword(e.target.value)} placeholder="Contraseña"></input>
 
        <button as="button"  class="botInicio" primario type="submit">Iniciar Sesión</button>
         
--- src/register.js
+++ src/register.js
@@ -1,37 +1,93 @@
-import React from "react";
+import React, {useState} from "react";
 import Helmet from "react-helmet";
-import logoRegistro from './Logo_Registro.svg'
+import logoRegistro from './Logo_Registro.svg';
 import './App.css';
+import axios from './instances/axiosInstance';
+import { useNavigate } from 'react-router-dom';
+
 const Registro=()=>{
+
+const [counterId, changeCounter] = useState("User");
+const [counter, changeNumber] = useState(4)
+const [name, changeName] = useState("");
+const [mail, changeMail] = useState("");
+const [gender, changeGender] = useState(false);
+const [phone, changePhone] = useState("");
+const [password, changePassword] = useState("");
+let navigate = useNavigate();
     return(
         <>
         <Helmet>
             <title>Registro</title>
         </Helmet>
-        <img class="logo_grande" src={logoRegistro} style={{marginTop:'0px'}}/>
-        <div style={{ position:'absolute', marginTop:'370px', alignItems:'center'}} >
+        <form>
+        <img class="logo_grande" src={logoRegistro} style={{marginTop:'-60px'}}/>
+        <div style={{ position:'absolute', left: '700px', alignItems:'center'}} >
             <input class="inputs"style={{display:'flex',alignItems:'flex-start', padding:'8px 12px', width:'471px',height:'40px', background:'#ffffff'}} 
-            type="email" placeholder="Correo*" required></input>
+            type="text" onChange={e => changeName(e.target.value)} placeholder="Nombre*" required ></input>
+
+            <input class="inputs"style={{marginTop:'40px', display:'flex',alignItems:'flex-start', padding:'8px 12px', width:'471px',height:'40px', background:'#ffffff'}} 
+x            type="email" onChange={e => changeMail(e.target.value)} placeholder="Correo*" required></input>
+
             <br></br>
-            <select id="genP"class="inputs" style={{alignItems:'center', padding:'8px 12px', width:'220px',height:'40px', background:'#ffffff'}}>
+            <select id="genP"class="inputs" onChange={e => changeGender(true)} style={{alignItems:'center', padding:'8px 12px', width:'220px',height:'40px', background:'#ffffff'}}>
                 <option value="" disabled selected>Género</option>
                 <option value="H">Hombre</option>
                 <option value="M">Mujer</option>
             </select>
+
             <input class="inputs" style={{marginTop:'40px',alignItems:'flex-start', padding:'8px 12px', width:'220px',height:'40px', background:'#ffffff', marginLeft:'31px'}}
-             type="tel" placeholder="Teléfono*" pattern="[0-11]{9}" required></input>
+             type="number" min="1111111111" max="9999999999" onInput="validity.valid||(value='');" onChange={e => changePhone(e.target.value)} placeholder="Teléfono*" pattern="[0-11]{9}" required></input>
             <br></br>
-            <input class="inputs" style={{ marginTop:'60px',display:'flex',alignItems:'flex-start', padding:'8px 12px', width:'471px',height:'40px', background:'#ffffff'}}
+
+            <input class="inputs" id = "con1" style={{ marginTop:'60px',display:'flex',alignItems:'flex-start', padding:'8px 12px', width:'471px',height:'40px', background:'#ffffff'}}
             type="password" placeholder="Contraseña*" required></input>
             <br></br>
-            <input class="inputs" style={{ marginTop:'40px',display:'flex',alignItems:'flex-start', padding:'8px 12px', width:'471px',height:'40px', background:'#ffffff'}}
-            type="password" placeholder="Confirmar Contraseña*" required></input>
+
+            <input class="inputs" id = "con2" style={{ marginTop:'40px',display:'flex',alignItems:'flex-start', padding:'8px 12px', width:'471px',height:'40px', background:'#ffffff'}}
+            type="password" onChange={e => changePassword(e.target.value)} placeholder="Confirmar Contraseña*" required></input>
             
 
-        <button class="botInicio">Confirmar</button>
+            <button class="botInicio" onClick={() => {changeNumber(counter+1); navigate("/");}}>Confirmar</button>
+            <div style={{ marginTop:'20px'}}></div>
        
         </div>
+        </form>
+        
     </>
     );
+
+function postInfo() {
+
+    /* axios({
+            method: 'post',
+            url: 'users',
+            data: {
+                client_id: counterId + counter,
+                email: "test2@gmail.com",
+                password: "89893898936",
+                genero: true,
+                telefono: "3143341001",
+                nombre: "Alvaro Tovar"
+            
+            }
+          }); */
+    
+    axios.post('users', {
+        client_id: counterId + counter,
+        email: mail,
+        password: password,
+        genero: true,
+        telefono: phone,
+        nombre: name,
+    })
+    .then((response) => {
+        console.log(response);
+    }, (error) => {
+        console.log(error);
+    });      
+
+    }
+
 }
 export default Registro;
